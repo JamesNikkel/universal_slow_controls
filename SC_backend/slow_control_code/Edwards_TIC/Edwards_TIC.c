5aedf95eaad6e6a899f926f600459a8ae5e6f903
James Nikkel <james@james-nikkels-macbook-pro.local>
2014-03-10 07:40:20 +0000

    Update to Edwards controller to control gauges and a bit of a clean up. -James

--- SC_backend/slow_control_code/Edwards_TIC/Edwards_TIC.c
+++ SC_backend/slow_control_code/Edwards_TIC/Edwards_TIC.c
@@ -56,9 +56,11 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	sprintf(cmd_string, "?V914%c", CR);
       else if (s_s->num == 3)
 	sprintf(cmd_string, "?V915%c", CR);
+      else if (s_s->num == 4)
+	sprintf(cmd_string, "?V934%c", CR);
       else
 	{
-	  fprintf(stderr, "Gauge number must be 1, 2, or 3, not %d.\n", s_s->num);
+	  fprintf(stderr, "Gauge number must be 1, 2, 3, or 4 not %d.\n", s_s->num);
 	  return(1);
 	}
       
@@ -114,32 +116,32 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 #define _def_set_sensor
 int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s)
 {
+  int        object_ID;      /// Command Object ID from the controller manual
+  int        set_value;
   char       cmd_string[64];
   char       ret_string[64];
   int        ret_status;
 
-  if (strncmp(s_s->subtype, "Pump", 3) == 0)  // Set the control loop setpoint
+  
+  if (strncmp(s_s->subtype, "Pressure", 3) == 0)  // Set values for pressure gauge 1, 2, or 3.
     {
-      if (s_s->num < 1 || s_s->num > 2) // Check correct pump number
+      if (s_s->num == 1)
+	object_ID = 913;
+      else if (s_s->num == 2)
+	object_ID = 914;
+      else if (s_s->num == 3)
+	object_ID = 915;
+      else if (s_s->num == 4)
+	object_ID = 934;
+      else
 	{
-	  fprintf(stderr, "Pump number must be 1 (for turbo) or 2 (for backing), not %d.\n", s_s->num);
+	  fprintf(stderr, "Gauge number must be 1, 2, 3, or 4 not %d.\n", s_s->num);
 	  return(1);
 	}
+
+      set_value = closest_int(s_s->new_set_val);
       
-      if (s_s->num == 1)  //Turbo
-	{
-	  if (s_s->new_set_val > 0.5)   
-	    sprintf(cmd_string, "!C904 1%c", CR);   // On
-	  else
-	    sprintf(cmd_string, "!C904 0%c", CR);   // Off
-	}
-      else        //  Backing
- 	{
-	  if (s_s->new_set_val > 0.5)   
-	    sprintf(cmd_string, "!C910 1%c", CR);   // On
-	  else
-	    sprintf(cmd_string, "!C910 0%c", CR);   // Off
-	}
+      sprintf(cmd_string, "!C%d %d%c", object_ID, set_value, CR); 
       
       query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
       msleep(1000);
@@ -151,17 +153,43 @@ int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s)
 	  return(1);
 	}
 
-      if (s_s->new_set_val > 0.5) 
+    }
+  
+  else if (strncmp(s_s->subtype, "Pump", 3) == 0)  // Set the control loop setpoint
+    {
+      if (s_s->num < 1 || s_s->num > 2) // Check correct pump number
 	{
-	  if (ret_status != 1)
-	    fprintf(stderr, "Trouble turning on pump! (%s)\n", s_s->name);
+	  fprintf(stderr, "Pump number must be 1 (for turbo) or 2 (for backing), not %d.\n", s_s->num);
+	  return(1);
 	}
-      else
-	{
-	  if (ret_status != 0)
-	    fprintf(stderr, "Trouble turning off pump! (%s)\n", s_s->name);
-	}	
       
+      if (s_s->num == 1)  //Turbo
+	{
+	  object_ID = 904;
+	}
+      else        //  Backing
+	{
+	  object_ID = 910;
+	}
+      
+      set_value = closest_int(s_s->new_set_val);   // 0 for off, 1 for on
+
+      sprintf(cmd_string, "!C%d %d%c", object_ID, set_value, CR); 
+      
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      msleep(1000);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+
+      if(sscanf(ret_string, "*%*s %d", &ret_status) != 1)   // 
+	{
+	  fprintf(stderr, "Bad return string: \"%s\" in set_sensor!\n", ret_string);
+	  return(1);
+	}
+
+      if (ret_status != set_value)
+	{
+	  fprintf(stderr, "Trouble setting pump %s to %d!\n", s_s->name, set_value);
+	}	
     }
   
   else       // Print an error if invalid subtype is entered
