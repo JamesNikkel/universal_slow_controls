6996905ffc346e3a7ded7a7e5ee55dca635891ae
James Nikkel <james.nikkel@gmail.com>
2015-06-01 15:12:17 +0100

    Update to LS335 code

--- SC_backend/slow_control_code/LS335_eth/LS335.c
+++ SC_backend/slow_control_code/LS335_eth/LS335.c
@@ -70,9 +70,14 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
   double     P_percent;
 
 
-  if (strncmp(s_s->subtype, "Temp", 1) == 0)  // Read out value for temperature from A,B,C or D.
+  if (strncmp(s_s->subtype, "Temp", 1) == 0)  // Read out value for temperature from A or B.
     {
-      sprintf(cmd_string, "INPUT? %c\n", int_to_Letter(s_s->num));
+      if (s_s->num < 1 || s_s->num > 2) // Checks correct sensor number
+	{
+	  fprintf(stderr, "%d is an incorrect value for num. Must be 1, or 2. \n", s_s->num);
+	  return(1);
+	}
+      sprintf(cmd_string, "KRDG? %c\n", int_to_Letter(s_s->num));
       query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
       msleep(200);
       query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
@@ -83,7 +88,42 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	  return(1);
 	}	
     }
+  else if (strncmp(s_s->subtype, "Heater", 1) == 0) // Read out heater power.
+    {
+      if (s_s->num < 1 || s_s->num > 2) // Checks correct Loop number
+	{
+	  fprintf(stderr, "%d is an incorrect value for num. Must be 1, or 2. \n", s_s->num);
+	  return(1);
+	}
+      sprintf(cmd_string, "HTR? %d\n", s_s->num);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      msleep(200);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      
+      if(sscanf(ret_string, "%lf", val_out) != 1)
+	{
+	  fprintf(stderr, "Bad return string: \"%s\" in read temperature!\n", ret_string);
+	  return(1);
+	}	
+    }
+  else if (strncmp(s_s->subtype, "Setpoint", 1) == 0) //Queries the setpoint 
+    {
+      if (s_s->num < 1 || s_s->num > 2) // Checks correct Loop number
+	{
+	  fprintf(stderr, "%d is an incorrect value for num. Must be 1, or 2. \n", s_s->num);
+	  return(1);
+	}
+      sprintf(cmd_string, "SETP? %d\n", s_s->num);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      msleep(200);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
 
+      if(sscanf(ret_string, "%lf", val_out) != 1)
+	{
+	  fprintf(stderr, "Bad return string: \"%s\" in read setpoint!\n", ret_string);
+	  return(1);
+	}	
+    }
   else       // Print an error if invalid subtype is entered
     {
       fprintf(stderr, "Wrong type for %s \n", s_s->name);
@@ -94,72 +134,60 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
   return(0);
 }
 
-#define _def_read_sensor
-int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_out)
-{
-    char   cmd_string[32];
-    char   val[32];                      
-    double P_percent;
-
-    if (strncmp(s_s->subtype, "temp", 3) == 0) // temperatures
-    {
-	if ((s_s->num < 1) || (s_s->num > 2))
-	{
-	    fprintf(stderr, "Channel number for %s must be 1 or 2 \n", s_s->name);
-	    return(1);
-	}
-	
-	sprintf(cmd_string, "KRDG? %c\r\n", int_to_Letter(s_s->num));
-	ibwrt(inst_dev, cmd_string, strlen(cmd_string));
-	msleep(100);  
-	ibrd(inst_dev, &val, 30);
-	sscanf(val, "%lf", val_out);	
-    }
-    else if (strncmp(s_s->subtype, "power", 3) == 0)   // heaters 
-    {
-	sprintf(cmd_string, "HTR?\r\n");
-	ibwrt(inst_dev, cmd_string, strlen(cmd_string));
-	msleep(100);  
-	ibrd(inst_dev, &val, 30);
-	sscanf(val, "%f", &P_percent);
-	*val_out = P_percent;
-    }
-    else
-    {
-	fprintf(stderr, "Wrong subtype for %s \n", s_s->name);
-	return(1);
-    }
-    return(0);
-}
-
 
 #define _def_set_sensor
 int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s)
 {
-    char cmd_string[64];
-    
-    if (s_s->new_set_val < 0 )
+  char       cmd_string[64];
+  char       ret_string[64];
+  double     ret_val;
+
+  if (strncmp(s_s->subtype, "Temp", 4) == 0)  // Set the control loop setpoint
     {
-	fprintf(stderr, "Setpoint for %s must be positive \n", s_s->name);
-	return(1);
-    }
-    if (strncmp(s_s->subtype, "setpoint", 3) == 0)     // closed loop setpoint
-    {
-	if ((s_s->num < 1) || (s_s->num > 2))
+      if (s_s->new_set_val < 0 ) // check valid value for Temp (>0)
 	{
-	    fprintf(stderr, "Loop number for %s must be 1 or 2 \n", s_s->name);
-	    return(1);
+	  fprintf(stderr, "%f is an incorrect temperature. Temp must be greater than 0... You Fool! \n", s_s->new_set_val);
+	  return(1);
 	}
-	sprintf(cmd_string, "SETP %d,%f\n", s_s->num, s_s->new_set_val);
-	ibwrt(inst_dev, cmd_string, strlen(cmd_string));
-	msleep(500);
+
+    if (s_s->num < 1 || s_s->num > 2) // Checks correct Loop number
+	{
+	  fprintf(stderr, "%d is an incorrect value for num. Must be 1, or 2. \n", s_s->num);
+	  return(1);
+	}
+    
+      sprintf(cmd_string, "SETP %d,%f\n", s_s->num, s_s->new_set_val);
+    
+      write_tcp(inst_dev, cmd_string, strlen(cmd_string));
+      sleep(1);
+	
+      sprintf(cmd_string, "SETP? %d\n", s_s->num); //queries control loop setpoint
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      msleep(200);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+
+      if(sscanf(ret_string, "%lf", &ret_val) != 1)
+	{
+	  fprintf(stderr, "Bad return string: \"%s\" in read setpoint!\n", ret_string);
+	  return(1);
+	}
+	
+      if (s_s->new_set_val != 0)
+	if (fabs(ret_val - s_s->new_set_val)/s_s->new_set_val > 0.1)
+	  {
+	    fprintf(stderr, "New setpoint of: %f is not equal to read out value of %f\n", s_s->new_set_val, ret_val);
+	    return(1);
+	  }
     }
-    else
+
+  else       // Print an error if invalid subtype is entered
     {
-	fprintf(stderr, "Wrong subtype for %s \n", s_s->name);
-	return(1);
-    }
-    return(0);  
+      fprintf(stderr, "Wrong type for %s \n", s_s->name);
+      return(1);
+    } 
+
+  return(0);
 }
+
 
 #include "main.h"
