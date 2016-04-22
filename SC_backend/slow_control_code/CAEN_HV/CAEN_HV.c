2173e542dd4e51fc3b0a38e6fa6ae76c28af92da
James Nikkel <james.nikkel@gmail.com>
2016-04-22 16:59:06 -0400

    update

--- SC_backend/slow_control_code/CAEN_HV/CAEN_HV.c
+++ SC_backend/slow_control_code/CAEN_HV/CAEN_HV.c
@@ -9,10 +9,8 @@
 #include "SC_aux_fns.h"
 #include "SC_sensor_interface.h"
 
-#include "ethernet.h"
 #include "v65xx.h"
 #include "CAENComm.h"
-
 
 // This is the default instrument entry, but can be changed on the command line when run manually.
 // When called with the watchdog, a specific instrument is always given even if it is the same
@@ -21,63 +19,76 @@
 
 int inst_dev;
 
+int get_reading(double *val, int channel, char *subtype)
+{
+  // 
+
+   // Connection info: optical fiber over link #0, node #0, with VME address AAAA0000  
+  int32_t  connectionType = CAENComm_OpticalLink;
+  int32_t  link = 0;
+  int32_t  conet = 0;
+  uint32_t vme_address = 0xaaaa0000;
+  int32_t  handle;
+  
+  CAENComm_ErrorCode  ret=0;
+  
+  // Open the HV module for communication
+  ret = CAENComm_OpenDevice(connectionType, link, conet, vme_address, &handle);
+
+  // Break if the HV module could not be opened
+  if(ret < 0 ) {
+    printf("Could not connect to the HV unit \n");
+    CAENComm_CloseDevice(handle);
+    return 1;
+  }
+
+  uint16_t data=0;
+  uint8_t  ch;
+  ch = uint8_t(channel);
+
+  // Check whether board is in alarm state
+  CAENComm_Read16(handle, 0x58, &data); // Alarm?
+  fprintf(stdout, "Alarm=%d  ",data);
+  
+  if (type == 1)
+    {
+      CAENComm_Read16(handle, 0x88+(128*ch), &data) ;	// Read Vmon
+      *val = (double)data / 10.0;
+    }
+  else if (type == 1)
+    {
+      CAENComm_Read16(handle, 0x8c+(128*ch), &data) ;	// Read ImonH
+      *val = (double)data / 20.0;
+    }
+  else
+    {
+      fprintf(stderr, "type 1 for voltage, 2 for current");	
+      return(1);
+      }
+  return(0);
+}
+
 
 #define _def_set_up_inst
 int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)    
 {
-  if ((inst_dev = connect_tcp(i_s)) < 0)
-    {
-      fprintf(stderr, "Connect failed. \n");
-      my_signal = SIGTERM;
-      return(1);
-    }
   return(0);
 }
 
 #define _def_clean_up_inst
 void clean_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
 {
-  close(inst_dev);
+  return(0);
 }
 
 #define _def_read_sensor
 int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_out)
 {
-  // returns the temp (which_sens_type == 1)
-  // or the heater power (which_sens_type == 2)
+  sleep(5);
 
-  char       cmd_string[64];
-  char       ret_string[64];             
-  long       ADC_counts;
-
-  sleep(1);
-  
-  if (s_s->num < 0 || s_s->num > 15)
-    {
-      fprintf(stderr, "Channel must be between 0 and 5 (inclusive), not %d.\n", s_s->num);
-      return(1);
-    }
-
-  sprintf(cmd_string, "%d\n", s_s->num);
-
-  query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-  msleep(200);
-  query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-
-  if (sscanf(ret_string, "%ld", &ADC_counts) !=1)
-    {
-      fprintf(stderr, "Bad return string: \"%s\" \n", ret_string);
-      return(1);
-    }
+  if (get_reading(*val_out, s_s->num, s_s->subtype))
+    return(1);
     
-  if (ADC_counts < 0)
-    {
-      fprintf(stderr, "Bad return value: \"%s\" \n", ret_string);
-      return(1);
-    }
-
-  *val_out = ( s_s->parm1 + (double)ADC_counts ) * s_s->parm2;  // standard conversion where parm1 is offset in ADC counts and parm2 is linear term
-
   return(0);
 }
 
