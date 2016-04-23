a80d176ecbd7c4fdbcaf656d44889171c81f34e8
James Nikkel <james.nikkel@rhul.ac.uk>
2016-04-23 18:09:44 -0400

    update

--- SC_backend/slow_control_code/CAEN_HV/CAEN_HV.c
+++ SC_backend/slow_control_code/CAEN_HV/CAEN_HV.c
@@ -28,6 +28,59 @@ void clean_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
 {
 }
 
+
+#define _def_set_sensor
+int set_sensor(struct inst_struct *i_s, struct sensor_struct *s_s)
+{
+  int32_t  connectionType = CAENComm_OpticalLink;
+  int32_t  link = 0;
+  int32_t  conet = 0;
+  uint32_t vme_address = 0xaaaa0000;
+  int32_t  handle;
+  uint16_t data=0;
+  uint8_t  ch;
+
+  CAENComm_ErrorCode  ret=0;
+  
+  // Open the HV module for communication
+  ret = CAENComm_OpenDevice(connectionType, link, conet, vme_address, &handle);
+
+  // Break if the HV module could not be opened
+  if(ret < 0 ) {
+    fprintf(stderr, "Could not connect to the HV unit ($s)\n", i_s->name);
+    CAENComm_CloseDevice(handle);
+    return(1);
+  }
+
+  ch = (uint8_t)s_s->num;   // get the channel number from the sensor structure
+  
+  if (strncmp(s_s->subtype, "Vset", 4) == 0)  // get the specific data from the sensor structure
+    {
+      CAENComm_Write16(handle, 0x80+(128*ch),  (uint16_t)(s_s->new_set_val*10)) ;	// Set voltage
+      sleep(1);
+      CAENComm_Read16(handle, 0x80+(128*ch), &data) ;	// Read Vmon
+      *val_out = (double)data / 10.0;
+    }
+  else if (strncmp(s_s->subtype, "Power", 1) == 3)
+    {
+      if (s_s->new_set_val > 0.5)
+	CAENComm_Write16(handle, 0x80+(128*ch), (uint16_t)1) ;	// Turn channel on
+      else
+	CAENComm_Write16(handle, 0x80+(128*ch), (uint16_t)0) ;	// Turn channel off
+    }
+  else
+    {
+      fprintf(stderr, "No such subtype %s in %s. \n", s_s->subtype, i_s->name);	
+      CAENComm_CloseDevice(handle);
+      return(1);
+    }
+  
+  CAENComm_CloseDevice(handle);  
+  return(0);
+
+}
+
+
 #define _def_read_sensor
 int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_out)
 {
@@ -62,39 +115,39 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
       fprintf(stderr, "%s alarm = %d\n", i_s->name, data);
     }
   
-  if (strncmp(s_s->subtype, "Vmon", 1) == 0)  // get the specific data from the sensor structure
+  if (strncmp(s_s->subtype, "Vmon", 4) == 0)  // get the specific data from the sensor structure
     {
       CAENComm_Read16(handle, 0x88+(128*ch), &data) ;	// Read Vmon
       *val_out = (double)data / 10.0;
     }
-  else if (strncmp(s_s->subtype, "ImonH", 1) == 0) 
+  else if (strncmp(s_s->subtype, "ImonH", 5) == 0) 
     {
       CAENComm_Read16(handle, 0x8c+(128*ch), &data) ;	// Read ImonH
       *val_out = (double)data / 20.0;
     }
-  else if (strncmp(s_s->subtype, "ImonL", 1) == 0) 
+  else if (strncmp(s_s->subtype, "ImonL", 5) == 0) 
     {
       CAENComm_Read16(handle, 0xb8+(128*ch), &data) ;	// Read ImonL
       *val_out = (double)data / 20.0;
     }
-  else if (strncmp(s_s->subtype, "Temp", 1) == 0) 
+  else if (strncmp(s_s->subtype, "Temperature", 4) == 0) 
     {
       CAENComm_Read16(handle, 0xb0+(128*ch), &data) ;	// Read temp
       *val_out = (double)data;
     }
-  else if (strncmp(s_s->subtype, "Status", 1) == 0) 
+  else if (strncmp(s_s->subtype, "Status", 4) == 0) 
     {
       CAENComm_Read16(handle, 0x94+(128*ch), &data) ;	// Read Ch Status
       *val_out = (double)data;
     }
-  else if (strncmp(s_s->subtype, "Power", 1) == 0) 
+  else if (strncmp(s_s->subtype, "Power", 3) == 0) 
     {
       CAENComm_Read16(handle, 0x90+(128*ch), &data) ;	// Read Power
       *val_out = (double)data;
     }
   else
     {
-      fprintf(stderr, "No such subtype in %s. \n", i_s->name);	
+      fprintf(stderr, "No such subtype %s in %s. \n", s_s->subtype, i_s->name);	
       CAENComm_CloseDevice(handle);
       return(1);
     }
