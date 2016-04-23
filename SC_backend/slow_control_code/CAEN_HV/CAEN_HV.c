56d396306811176b3d8630d6911bfd8a9b43a012
James Nikkel <james.nikkel@rhul.ac.uk>
2016-04-23 07:29:14 -0400

    update

--- SC_backend/slow_control_code/CAEN_HV/CAEN_HV.c
+++ SC_backend/slow_control_code/CAEN_HV/CAEN_HV.c
@@ -17,57 +17,6 @@
 // as the default. 
 #define INSTNAME "CAEN_HV"
 
-int inst_dev;
-
-int get_reading(double *val, int channel, char *subtype)
-{
-  // 
-
-   // Connection info: optical fiber over link #0, node #0, with VME address AAAA0000  
-  int32_t  connectionType = CAENComm_OpticalLink;
-  int32_t  link = 0;
-  int32_t  conet = 0;
-  uint32_t vme_address = 0xaaaa0000;
-  int32_t  handle;
-  
-  CAENComm_ErrorCode  ret=0;
-  
-  // Open the HV module for communication
-  ret = CAENComm_OpenDevice(connectionType, link, conet, vme_address, &handle);
-
-  // Break if the HV module could not be opened
-  if(ret < 0 ) {
-    printf("Could not connect to the HV unit \n");
-    CAENComm_CloseDevice(handle);
-    return 1;
-  }
-
-  uint16_t data=0;
-  uint8_t  ch;
-  ch = uint8_t(channel);
-
-  // Check whether board is in alarm state
-  CAENComm_Read16(handle, 0x58, &data); // Alarm?
-  fprintf(stdout, "Alarm=%d  ",data);
-  
-  if (type == 1)
-    {
-      CAENComm_Read16(handle, 0x88+(128*ch), &data) ;	// Read Vmon
-      *val = (double)data / 10.0;
-    }
-  else if (type == 1)
-    {
-      CAENComm_Read16(handle, 0x8c+(128*ch), &data) ;	// Read ImonH
-      *val = (double)data / 20.0;
-    }
-  else
-    {
-      fprintf(stderr, "type 1 for voltage, 2 for current");	
-      return(1);
-      }
-  return(0);
-}
-
 
 #define _def_set_up_inst
 int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)    
@@ -85,10 +34,71 @@ void clean_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
 int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_out)
 {
   sleep(5);
+  
+  int32_t  connectionType = CAENComm_OpticalLink;
+  int32_t  link = 0;
+  int32_t  conet = 0;
+  uint32_t vme_address = 0xaaaa0000;
+  int32_t  handle;
+  
+  CAENComm_ErrorCode  ret=0;
+  
+  // Open the HV module for communication
+  ret = CAENComm_OpenDevice(connectionType, link, conet, vme_address, &handle);
 
-  if (get_reading(*val_out, s_s->num, s_s->subtype))
+  // Break if the HV module could not be opened
+  if(ret < 0 ) {
+    fprintf(stderr, "Could not connect to the HV unit ($s)\n", i_s->name);
+    CAENComm_CloseDevice(handle);
     return(1);
-    
+  }
+
+  uint16_t data=0;
+  uint8_t  ch;
+  ch = uint8_t(channel);
+
+  // Check whether board is in alarm state
+  CAENComm_Read16(handle, 0x58, &data); // Alarm?
+  fprintf(stdout, "Alarm=%d  ",data);
+  
+  if (strncmp(s_s->subtype, "Vmon", 1) == 0) 
+    {
+      CAENComm_Read16(handle, 0x88+(128*ch), &data) ;	// Read Vmon
+      *val_out = (double)data / 10.0;
+    }
+  else if (strncmp(s_s->subtype, "ImonH", 1) == 0) 
+    {
+      CAENComm_Read16(handle, 0x8c+(128*ch), &data) ;	// Read ImonH
+      *val_out = (double)data / 20.0;
+    }
+  else if (strncmp(s_s->subtype, "ImonL", 1) == 0) 
+    {
+      CAENComm_Read16(handle, 0xb8+(128*ch), &data) ;	// Read ImonL
+      *val_out = (double)data / 20.0;
+    }
+  else if (strncmp(s_s->subtype, "Temp", 1) == 0) 
+    {
+      CAENComm_Read16(handle, 0xb0+(128*ch), &data) ;	// Read temp
+      *val_out = (double)data;
+    }
+  else if (strncmp(s_s->subtype, "Status", 1) == 0) 
+    {
+      CAENComm_Read16(handle, 0x94+(128*ch), &data) ;	// Read Ch Status
+      *val_out = (double)data;
+    }
+  else if (strncmp(s_s->subtype, "Status", 1) == 0) 
+    {
+      CAENComm_Read16(handle, 0x90+(128*ch), &data) ;	// Read Power
+      *val_out = (double)data;
+    }
+  else
+    {
+      fprintf(stderr, "No such subtype in %s. \n" i_s->name);	
+      CAENComm_CloseDevice(handle);
+      return(1);
+    }
+  
+  CAENComm_CloseDevice(handle);  
   return(0);
 }
 
