532c7bc88d512c826aaab1abdfe7d53c34e1a755
James Nikkel <james@Jamess-MBP-5.central.yale.internal>
2016-07-20 11:16:42 -0400

    update

--- SC_backend/slow_control_code/Arduino_ADC/Arduino_ADC.c
+++ SC_backend/slow_control_code/Arduino_ADC/Arduino_ADC.c
@@ -79,7 +79,8 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
   char       ret_string[64];             
   long       ADC_counts;
   int        DIO_val;
-    
+  float      pthVal;
+  
   if (strncmp(s_s->subtype, "DIO", 3) == 0)  // Do DIO reads
     {
        if (s_s->num < 22 || s_s->num > 53)
@@ -131,6 +132,48 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 	}
 
       *val_out = ( s_s->parm1 + (double)ADC_counts ) * s_s->parm2;  // standard conversion where parm1 is offset in ADC counts and parm2 is linear term
+    }
+  else if  (strncmp(s_s->subtype, "Temp", 4) == 0)
+    {
+      sprintf(cmd_string, "RDT\n");
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      msleep(200);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+
+      if (sscanf(ret_string, "%f", &pthVal) !=1)
+	{
+	  fprintf(stderr, "Bad return string: \"%s\" \n", ret_string);
+	  return(1);
+	}
+      *val_out = pthVal;
+    }
+   else if  (strncmp(s_s->subtype, "Humidity", 3) == 0)
+    {
+      sprintf(cmd_string, "RDH\n");
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      msleep(200);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+
+      if (sscanf(ret_string, "%f", &pthVal) !=1)
+	{
+	  fprintf(stderr, "Bad return string: \"%s\" \n", ret_string);
+	  return(1);
+	}
+      *val_out = pthVal;
+    }
+   else if  (strncmp(s_s->subtype, "Pres", 4) == 0)
+    {
+      sprintf(cmd_string, "RDP\n");
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+      msleep(200);
+      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
+
+      if (sscanf(ret_string, "%f", &pthVal) !=1)
+	{
+	  fprintf(stderr, "Bad return string: \"%s\" \n", ret_string);
+	  return(1);
+	}
+      *val_out = pthVal;
     }
   else       // Print an error if invalid subtype is entered
     {
