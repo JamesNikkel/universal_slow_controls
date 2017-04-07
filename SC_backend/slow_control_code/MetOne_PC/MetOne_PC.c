86cb780e52e80cdb64d73cd76eb9f8af92e0b356
James Nikkel <james.nikkel@gmail.com>
2017-04-07 09:33:21 -0400

    huzzah!

--- SC_backend/slow_control_code/MetOne_PC/MetOne_PC.c
+++ SC_backend/slow_control_code/MetOne_PC/MetOne_PC.c
@@ -35,7 +35,7 @@ int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
   
   sprintf(cmd_string, "U12%c", CR);   // start comms
   query_tcp(inst_dev, cmd_string,  strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-  fprintf(stdout, "Startup:\n %s \n", ret_string);
+  //fprintf(stdout, "Startup:\n %s \n", ret_string);
   //read_tcp(inst_dev, ret_string, sizeof(ret_string)/sizeof(char));
   //fprintf(stdout, "Startup:\n %s \n", ret_string);
   //read_tcp(inst_dev, ret_string, sizeof(ret_string)/sizeof(char));
@@ -43,7 +43,7 @@ int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
 
   sprintf(cmd_string, "T30%c", CR);   // start comms
   query_tcp(inst_dev, cmd_string,  strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-  fprintf(stdout, "Set time:\n %s \n", ret_string);
+  //fprintf(stdout, "Set time:\n %s \n", ret_string);
   //read_tcp(inst_dev, ret_string, sizeof(ret_string)/sizeof(char));
   //fprintf(stdout, "Set time:\n %s \n", ret_string);
   //read_tcp(inst_dev, ret_string, sizeof(ret_string)/sizeof(char));
@@ -80,7 +80,7 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
 
   sprintf(cmd_string, "S");   // start counting
   query_status = query_tcp(inst_dev, cmd_string,  strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-  fprintf(stdout, "S Return string:\n %s \n", ret_string);
+  //fprintf(stdout, "S Return string:\n %s \n", ret_string);
   //read_tcp(inst_dev, ret_string, sizeof(ret_string)/sizeof(char));
   //fprintf(stdout, "S Return string:\n %s \n", ret_string);
   sleep(35);
@@ -90,7 +90,7 @@ int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_
       sprintf(cmd_string, "L");   // list output
       query_status = query_tcp(inst_dev, cmd_string,  strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
       read_tcp(inst_dev, ret_string, sizeof(ret_string)/sizeof(char));
-      fprintf(stdout, "L Return string:\n %s \n", ret_string);
+      //fprintf(stdout, "L Return string:\n %s \n", ret_string);
 
       if(sscanf(ret_string, " L %*d/%*d/%*d,%*d:%*d:%*d,%*d,%*f,%d,%*f,%d,%*s", &return_int1, &return_int2) == 2)
 	tries = 20;
