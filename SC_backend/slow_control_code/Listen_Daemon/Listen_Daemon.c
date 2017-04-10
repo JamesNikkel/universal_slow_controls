c5c3ead79350dde4d2f2ece139cec4ed26b241cf
James Nikkel <james@Jamess-MBP-5.central.yale.internal>
2017-04-10 14:15:33 -0400

    update`

--- SC_backend/slow_control_code/Listen_Daemon/Listen_Daemon.c
+++ SC_backend/slow_control_code/Listen_Daemon/Listen_Daemon.c
@@ -10,103 +10,135 @@
 #include "SC_sensor_interface.h"
 
 #include "ethernet.h"
-
-// This is the default instrument entry, but can be changed on the command line when run manually.
-// When called with the watchdog, a specific instrument is always given even if it is the same
-// as the default. 
+ 
 #define INSTNAME "Listen_Daemon"
 
-int inst_dev;
 
-#define _def_set_up_inst
-int set_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)    
+int setup(struct inst_struct *i_s)
 {
-  if ((inst_dev = connect_tcp(i_s)) < 0)
-    {
-      fprintf(stderr, "Connect failed. \n");
-      my_signal = SIGTERM;
-      return(1);
-    }
+  int sockfd, newsockfd, portno;
+  socklen_t clilen;
+  char buffer[256];
+  struct sockaddr_in serv_addr, cli_addr;
+  int n;
+  if (argc < 2) {
+    fprintf(stderr,"ERROR, no port provided\n");
+    exit(1);
+  }
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) 
+    error("ERROR opening socket");
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  portno = atoi(argv[1]);
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+  if (bind(sockfd, (struct sockaddr *) &serv_addr,
+	   sizeof(serv_addr)) < 0) 
+    error("ERROR on binding");
+  listen(sockfd,5);
+  clilen = sizeof(cli_addr);
+  newsockfd = accept(sockfd, 
+		     (struct sockaddr *) &cli_addr, 
+		     &clilen);
+  if (newsockfd < 0) 
+    error("ERROR on accept");
+  bzero(buffer,256);
+  n = read(newsockfd,buffer,255);
+  if (n < 0) error("ERROR reading from socket");
+  printf("Here is the message: %s\n",buffer);
+  n = write(newsockfd,"I got your message",18);
+  if (n < 0) error("ERROR writing to socket");
+  close(newsockfd);
+  close(sockfd);
   return(0);
 }
 
-#define _def_clean_up_inst
-void clean_up_inst(struct inst_struct *i_s, struct sensor_struct *s_s_a)
+void listen_loop(struct inst_struct *i_s)
 {
-  close(inst_dev);
-}
 
-
-
-#define _def_read_sensor
-int read_sensor(struct inst_struct *i_s, struct sensor_struct *s_s, double *val_out)
-{
   
-  char       ret_string[64];             
-  long       ADC_counts;
-  int        DIO_val;
-    
-  if (strncmp(s_s->subtype, "DIO", 3) == 0)  // Do DIO reads
-    {
-       if (s_s->num < 22 || s_s->num > 53)
-	{
-	  fprintf(stderr, "Channel must be between 22 and 53 (inclusive), not %d.\n", s_s->num);
-	  return(1);
-	}
-
-      sprintf(cmd_string, "RD %d\n", s_s->num);
-
-      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-      msleep(200);
-      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-     
-      if (sscanf(ret_string, "%d", &DIO_val) !=1)
-	{
-	  fprintf(stderr, "Bad return string: \"%s\" \n", ret_string);
-	  return(1);
-	}
-      if (DIO_val > 0)
-	*val_out = 1;
-      else
-	*val_out = 0; 
-    }
-  else if  (strncmp(s_s->subtype, "AN", 2) == 0)  // Do analogue reads
-    {
-      if (s_s->num < 0 || s_s->num > 15)
-	{
-	  fprintf(stderr, "Channel must be between 0 and 15 (inclusive), not %d.\n", s_s->num);
-	  return(1);
-	}
-
-      sprintf(cmd_string, "RA %d\n", s_s->num);
-
-      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-      msleep(200);
-      query_tcp(inst_dev, cmd_string, strlen(cmd_string), ret_string, sizeof(ret_string)/sizeof(char));
-
-      if (sscanf(ret_string, "%ld", &ADC_counts) !=1)
-	{
-	  fprintf(stderr, "Bad return string: \"%s\" \n", ret_string);
-	  return(1);
-	}
-    
-      if (ADC_counts < 0)
-	{
-	  fprintf(stderr, "Bad return value: \"%s\" \n", ret_string);
-	  return(1);
-	}
-
-      *val_out = ( s_s->parm1 + (double)ADC_counts ) * s_s->parm2;  // standard conversion where parm1 is offset in ADC counts and parm2 is linear term
-    }
-  else       // Print an error if invalid subtype is entered
-    {
-      fprintf(stderr, "Wrong type for %s \n", s_s->name);
-      return(1);
-    } 
-
-  sleep(1);
-
-  return(0);
+  return;
 }
 
-#include "main.h"
+
+
+int main (int argc, char *argv[])
+{
+  char                   **my_argv;
+  char                   inst_name[16];
+  int                    i;
+  struct inst_struct     this_inst;
+  struct sensor_struct   *all_sensors;
+
+  // save restart arguments
+  my_argv = malloc(sizeof(char *) * (argc + 1));
+  for (i = 0; i < argc; i++) 
+    my_argv[i] = strdup(argv[i]);
+   
+  my_argv[i] = NULL;   
+    
+  sprintf(db_conf_file, DEF_DB_CONF_FILE);
+
+  parse_CL_for_string(argc,  argv, INSTNAME, inst_name);
+  read_mysql_inst_struct(&this_inst, inst_name);
+    
+  // detach current process
+  daemonize(this_inst.name);
+    
+  my_signal = 0;
+  // ignore these signals 
+  signal(SIGINT, SIG_IGN);
+  signal(SIGQUIT, SIG_IGN);    
+  // install signal handler
+  signal(SIGHUP, handler);
+  signal(SIGINT, handler);
+  signal(SIGQUIT, handler);
+  signal(SIGTERM, handler);
+
+  register_inst(&this_inst);
+
+  if (set_up_inst(&this_inst, all_sensors))
+    {
+      msleep(1000);
+      my_signal = SIGHUP;
+    }
+  mysql_inst_run_status(&this_inst);
+   
+  while (my_signal == 0)     //  main loop here!
+    {
+      listen_loop(&this_inst);
+      mysql_inst_run_status(&this_inst);
+    }
+    
+  /////////////  Clean up if we get a signal
+  clean_up_inst(&this_inst, all_sensors);
+
+  unregister_inst(&this_inst);
+  free(all_sensors);
+
+  if (my_signal == SIGHUP)         ///  restart called
+    {
+      long fd;
+      // close all files before restart
+      for (fd = sysconf(_SC_OPEN_MAX); fd > 2; fd--) 
+	{
+	  int flag;
+	  if ((flag = fcntl(fd, F_GETFD, 0)) != -1)
+	    fcntl(fd, F_SETFD, flag | FD_CLOEXEC);
+	}
+      sleep(2);
+      execv(my_argv[0], my_argv);
+      fprintf(stderr, "execv() failed.");
+      exit(1);
+    }
+
+  for (i = 0; my_argv[i] != NULL; i++)
+    free(my_argv[i]);
+    
+  free(my_argv);
+
+  exit(0);
+}
+
+
