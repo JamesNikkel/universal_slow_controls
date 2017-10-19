5ed500d7f5326a11704b6390e5738c554724ac4c
James Nikkel <james@Jamess-MBP-5.wireless.yale.internal>
2017-10-19 14:54:16 -0400

    added stuping includes.... psi

--- SC_backend/slow_control_code/L_Scanner/test_scan.c
+++ SC_backend/slow_control_code/L_Scanner/test_scan.c
@@ -1,4 +1,14 @@
 
+#include <stdio.h>
+
+#include <sys/select.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
 
 
 int inst_dev_1;
